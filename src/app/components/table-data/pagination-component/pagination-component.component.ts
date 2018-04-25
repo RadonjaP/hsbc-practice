ca2b48f037ebe0ecf914ee54415feddafcc4eb24
RadonjaPrelevic <rudonja8@gmail.com>
2018-04-25 14:35:13 +0200

    import fixed

--- src/app/components/table-data/pagination-component/pagination-component.component.ts
+++ src/app/components/table-data/pagination-component/pagination-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'pagination-component',
